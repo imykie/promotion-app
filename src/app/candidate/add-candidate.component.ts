072c0d84c1b94a673195857d8bcdc30f9ea89bf1
Olatunde Temitope <tupskey@gmail.com>
2020-02-25 15:20:05 +0100

    added image

--- src/app/candidate/add-candidate.component.ts
+++ src/app/candidate/add-candidate.component.ts
@@ -88,104 +88,102 @@ export class AddCandidateComponent implements OnInit{
     Submitted = false
     AddCandidate: FormGroup
     Department: any = [
-        'Agricultural Economics & Extension', 'Agricultural Economics',   'Agricultural and Environmental Engineering', 'Agricultural Extension and Rural Sociology', 'Anaesthesia and Intensive Care', 'Anatomy and Cell Biology',
-        'Animal Science','Archeaology','Architecture',
-        'Biochemistry',
-        'Botany',
-        'Building',
-        'Business Law',
-        'Chemical Engineering',
-        'Chemical Pathology',
-        'Chemistry',
-        'Civil Engineering',
-        'Clinical Pharmacy and Pharmacy Administration',
-        'Community Health and Nutrition',
-        'Computer Science and Engineering',
-        'Continuing Education',
-        'Crop production',
-        'Demography and Social Statistics',
-        'Dermatology and Venerology',
-        'Dramatic Arts',
-        'Drug Research and Production Unit',
-        'Economics',
-        'Educational Administration and Planning',
-        'Educational Foundation and Counseling',
-        'Educational Technology',
-        'Electronic and Electrical Engineering',
-        'English Language',
-        'Environmental Health and Epidemiology',
-        'Estate Management',
-        'Fine and Applied Arts',
-        'Food Nutrition and Consumer Sciences',
-        'Food Science and Technology',
-        'Foreign Languages',
-        'Geography',
-        'Geology',
-        'Haematology and Immunology',
-        'History',
-        'Institute of Education',
-        'International Law',
-        'International Relations',
-        'Jurisprudence and Private Law',
-        'Linguistics',
-  'Linguistics and African Languages',
-        'Local Government Studies',
-        'Management and Accounting',
-        'Mathematics',
-        'Mechanical Engineering',
-        'Medical Microbiology and Parasitology',
-        'Medical Rehabilitation',
-        'Medicine',
-        'Mental Health',
-        'Materials Science & Engineering',
-        'Microbiology',
-        'Medical Pharmacology and Therapeutics',
-        'Morbid Anatomy and Forensic Medicine',
-        'Music','Nursing',
-        'Obstetrics, Gynaecology and Perimatology',
-        'Orthopaedic Surgery and Traumatology',
-        'Paediatrics and Child Health',
-        'Pharmaceutical Chemistry',
-        'Pharmaceutics',
-        'Pharmacognosy',
-        'Pharmacology',
-        'Philosophy',
-        'Physical and Health Education',
-        'Physics',
-        'Physiological Sciences',
-        'Plant Science',
-        'Political Science',
-        'Psychology',
-        'Public Administration',
-        'Public Law',
-        'Quantity Surveying',
-        'Radiology',
-        'Religious Studies',
-        'Sociology and Anthropology',
-        'Soil Science',
-        'Special Education and Curriculum Studies',
-        'Surgery',
-        'Urban and Regional Planning',
-        'Zoology'];
-    Level: any = ['Graduate Assistant','Lecuturer I', 'Lecturer II', ' Senior Lecturer', 'Reader/Associcate Professor', 'Professor'];
+      'Agricultural Economics & Extension', 'Agricultural Economics',   'Agricultural and Environmental Engineering', 'Agricultural Extension and Rural Sociology', 'Anaesthesia and Intensive Care', 'Anatomy and Cell Biology',
+      'Animal Science','Archeaology','Architecture',
+      'Biochemistry',
+      'Botany',
+      'Building',
+      'Business Law',
+      'Chemical Engineering',
+      'Chemical Pathology',
+      'Chemistry',
+      'Civil Engineering',
+      'Clinical Pharmacy and Pharmacy Administration',
+      'Community Health and Nutrition',
+      'Computer Science and Engineering',
+      'Continuing Education',
+      'Crop production',
+      'Demography and Social Statistics',
+      'Dermatology and Venerology',
+      'Dramatic Arts',
+      'Drug Research and Production Unit',
+      'Economics',
+      'Educational Administration and Planning',
+      'Educational Foundation and Counseling',
+      'Educational Technology',
+      'Electronic and Electrical Engineering',
+      'English Language',
+      'Environmental Health and Epidemiology',
+      'Estate Management',
+      'Fine and Applied Arts',
+      'Food Nutrition and Consumer Sciences',
+      'Food Science and Technology',
+      'Foreign Languages',
+      'Geography',
+      'Geology',
+      'Haematology and Immunology',
+      'History',
+      'Institute of Education',
+      'International Law',
+      'International Relations',
+      'Jurisprudence and Private Law',
+      'Linguistics',
+'Linguistics and African Languages',
+      'Local Government Studies',
+      'Management and Accounting',
+      'Mathematics',
+      'Mechanical Engineering',
+      'Medical Microbiology and Parasitology',
+      'Medical Rehabilitation',
+      'Medicine',
+      'Mental Health',
+      'Materials Science & Engineering',
+      'Microbiology',
+      'Medical Pharmacology and Therapeutics',
+      'Morbid Anatomy and Forensic Medicine',
+      'Music','Nursing',
+      'Obstetrics, Gynaecology and Perimatology',
+      'Orthopaedic Surgery and Traumatology',
+      'Paediatrics and Child Health',
+      'Pharmaceutical Chemistry',
+      'Pharmaceutics',
+      'Pharmacognosy',
+      'Pharmacology',
+      'Philosophy',
+      'Physical and Health Education',
+      'Physics',
+      'Physiological Sciences',
+      'Plant Science',
+      'Political Science',
+      'Psychology',
+      'Public Administration',
+      'Public Law',
+      'Quantity Surveying',
+      'Radiology',
+      'Religious Studies',
+      'Sociology and Anthropology',
+      'Soil Science',
+      'Special Education and Curriculum Studies',
+      'Surgery',
+      'Urban and Regional Planning',
+      'Zoology'];
+      Level: any = ['Graduate Assistant','Lecuturer I', 'Lecturer II', ' Senior Lecturer', 'Reader/Associcate Professor', 'Professor'];
     Faculty: any = ['Faculty of Agriculture','Faculty of Administration','Faculty of Law','Faculty of  Pharmacy','Faculty of Sciences',
     'Faculty of Technology',  'Faculty of Art','Faculty of Social-Sciences','Faculty of Enviromental Design and Management', 'Faculty of  Basic Medical Sciences',
-    'Faculty of Clinical Sciences','Faculty of Dentistry', 'Faculty of Education'];
-    
+    'Faculty of Clinical Sciences','Faculty of Dentistry', 'Faculty of Education',
+];
+
 
     get accessor(): FormArray{
         return <FormArray>this.AddCandidate.get('accessor')
     }
 
-    constructor(private fb: FormBuilder, private router: Router, private portalService: PortalService){
+    constructor(private fb: FormBuilder, private router: Router,
+       private portalService: PortalService){
         this.mainForm();
     }
 
-    ngOnInit(){
-        this.portalService.channel.bind('new-notification', data => {
-            console.log(data.message, data.date);
-        })
-    }
+    ngOnInit(){}
     mainForm(){
         this.AddCandidate = this.fb.group({
             surname: ['', Validators.required],
@@ -197,28 +195,29 @@ export class AddCandidateComponent implements OnInit{
             lev: ['',  Validators.required],
             date: ['', Validators.required],
             accessor: this.fb.array([ this.buildAccessors() ])
-        })  
+        })
     }
-    
+
 
     onSubmit(){
         this.Submitted = true;
-        if(!this.AddCandidate.valid){
-            return false
-        }else{
+        
            this.portalService.addCandidate(this.AddCandidate.value).subscribe(data => {
             if(data){
-                console.log(data); 
+                console.log(data);
+
             }
             else{
                 console.log('Invalid')
             }
         },(error) => {
                console.log(error)
-           }) 
-        }
+           })
+        
         console.log(this.AddCandidate.value)
-        this.router.navigate(['candidate-list'])
+        this.router.navigate(['candidate-list']);
+        
+
     }
 
     buildAccessors(): FormGroup{
@@ -226,7 +225,7 @@ export class AddCandidateComponent implements OnInit{
             accessorname:  ['', Validators.required],
             accessoremail:  ['', Validators.required],
             university:  ['', Validators.required],
-            status: 'invitationsent'
+            status: 'invitation sent'
         })
     }
 
@@ -245,7 +244,6 @@ export class AddCandidateComponent implements OnInit{
          onlySelf: true
         })
      }
-
 
      addAccessors(): void{
         this.accessor.push(this.buildAccessors());
